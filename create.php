6b2277146c84390f3929889daa65e6e427886517
Darryl <darrylmangibin@gmail.com>
2021-10-16 21:53:32 +0700

    Uploading file

--- create.php
+++ create.php
@@ -3,24 +3,56 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$errors = [];
+
+$title = '';
+$price = '';
+$description = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $title = $_POST['title'];
   $description = $_POST['description'];
   $price = $_POST['price'];
   $date = date('Y-m-d H:i:s');
 
-  $statement = $pdo->prepare(
-    "INSERT INTO products (title, image, description, price, create_date) 
-    VALUES (:title, :image, :description, :price, :date)"
-  );
 
-  $statement->bindValue(':title', $title);
-  $statement->bindValue(':image', '');
-  $statement->bindValue(':description', $description);
-  $statement->bindValue(':price', $price);
-  $statement->bindValue(':date', $date);
+  if (!$title) {
+    array_push($errors, 'Product title is required');
+  }
 
-  $statement->execute();
+  if (!$price) {
+    array_push($errors, 'Product price is required');
+  }
+
+  if (!is_dir('images')) {
+    mkdir('images');
+  }
+
+  if (empty($errors)) {
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+    if ($image) {
+      $imagePath = 'images/' . rand(1, 1000000000000) . '/' . $image['name'];
+
+      mkdir(dirname($imagePath));
+
+      move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+
+    $statement = $pdo->prepare(
+      "INSERT INTO products (title, image, description, price, create_date) 
+      VALUES (:title, :image, :description, :price, :date)"
+    );
+
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':image', $imagePath);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date);
+
+    $statement->execute();
+  }
 }
 ?>
 
@@ -32,6 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
@@ -42,7 +75,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
   <h1>Create new Product</h1>
 
-  <form action="" method="POST">
+  <?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger">
+      <?php foreach ($errors as $error) : ?>
+        <div><?php echo $error ?></div>
+      <?php endforeach ?>
+    </div>
+  <?php endif ?>
+
+  <form action="" method="POST" enctype="multipart/form-data">
     <div class="form-group">
       <label>Product Image</label>
       <br />
@@ -50,15 +91,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
     <div class="form-group">
       <label>Product Title</label>
-      <input type="text" class="form-control" name="title">
+      <input type="text" class="form-control" name="title" value="<?php echo $title ?>">
     </div>
     <div class="form-group">
       <label>Product Description</label>
-      <textarea class="form-control" name="description"></textarea>
+      <textarea class="form-control" name="description" value="<?php echo $description ?>"></textarea>
     </div>
     <div class="form-group">
       <label>Product price</label>
-      <input type="number" step=".01" class="form-control" name="price">
+      <input type="number" step=".01" class="form-control" name="price" value="<?php echo $price ?>">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
